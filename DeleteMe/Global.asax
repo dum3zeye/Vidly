4a730fafb416f1b7c8f09711b4a80277d05a411c
Jesse Lewis <jlewis@accentcontrols.com>
2019-03-17 10:56:13 -0500

    Restarted project due to issues caused from not selecting authorization when creating it.

--- DeleteMe/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeleteMe.MvcApplication" Language="C#" %>
---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ViewResult Index()
+        {
+            List<Customer> customers = Customers.ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = Customers.SingleOrDefault(c => c.Id == id);
+
+            return customer == null ? HttpNotFound() : (ActionResult)View(customer);
+        }
+
+        private IQueryable<Customer> Customers => _context.Customers.Include(c => c.MembershipType);
+    }
+}
